e900a2839cf78c85d79f329d623dd01d0e015ff2
elismrclsa <emarcelatfs@gmail.com>
2021-07-06 21:38:53 -0300

    2021

---
+++ 1001 - Extremamente Básico.py
@@ -0,0 +1,4 @@
+A = input()
+B = input()
+som = int(A) + int(B)
+print('X =', som)
---
+++ 1002 - Área do Círculo.py
@@ -0,0 +1,4 @@
+raio = input()
+area = float(raio) * float(raio) * 3.14159
+formatted_area = "{:.4f}".format(area)
+print('A=',formatted_area,sep='')
